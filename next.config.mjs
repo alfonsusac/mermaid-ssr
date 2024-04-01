47e0a068167657eee35ccb25780109e13ac34fcf
Alfonsus Ardani <alfonsusac@gmail.com>
2024-04-01 12:37:18 +0700

    added next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //...
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium']
+  }
+  //...
+}
 
 export default nextConfig;
