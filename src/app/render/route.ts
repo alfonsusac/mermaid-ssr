8d9333ed5fc7be8c29146a007e2b826e97ee3d31
Alfonsus Ardani <alfonsusac@gmail.com>
2024-04-01 18:53:22 +0700

    fix export

--- src/app/render/route.ts
+++ src/app/render/route.ts
@@ -33,48 +33,6 @@ export async function GET(request: NextRequest) {
   } finally {
     // browser.close()
   }
-
-
-  // const startRenderTime = performance.now()
-  // let result
-  // try {
-  //   result = await renderMermaid(
-  //     browser as any,
-  //     code,
-  //     "svg",
-  //     {
-  //       backgroundColor: backgroundColor ?? "",
-  //       mermaidConfig: {
-  //         htmlLabels: true,
-  //         fontFamily: "var(--font-inter)",
-  //         themeCSS: "HELLO YES",
-  //         ...cfg
-  //       },
-  //       myCSS: ".mermaid-js{font-size: 0.8em !important;}",
-  //     }
-  //   )
-  // } catch (error: any) {
-  //   const msg = error.message as string
-  //   const errormsg = msg.split('\n').slice(0,
-  //     msg.split('\n').findIndex(l => l.includes('@mermaid-js'))
-  //   ).join('\n')
-  //   console.log(errormsg)
-  //   return NextResponse.json({ status: errormsg })
-  // }
-  // // const endRenderTime = performance.now() // Get the end RenderTime
-  // // const executionRenderTime = endRenderTime - startRenderTime // Calculate the difference
-
-  // const svg = result.data.toString('utf-8').replace(`id="my-svg"`, `id="my-svg" class="mermaid-js"`)
-
-  // return NextResponse.json({
-  //   status: "ok",
-  //   timing: {
-  //     // browserStartMs: Math.round((executionBrowserStartTime ?? 0) * 100) / 100,
-  //     // renderMs: Math.round((executionRenderTime ?? 0) * 100) / 100,
-  //     // browserCloseMs: Math.round((executionBrowserCloseTime ?? 0) * 100) / 100,
-  //   },
-  //   svg,
-  // })
 }
 
 // http://localhost:3010/render?code=graph%20TD%3B%0A%20%20%20%20A%5BSquare%20Rect%5D%20--%3E%7CLink%20text%7C%20B(Round%20Rect)%3B%0A%20%20%20%20A%20--%3E%20C%7BDecision%7D%3B%0A%20%20%20%20B%20--%3E%20D%7BDecision%7D%3B%0A%20%20%20%20C%20--%3E%7CYes%7C%20D%3B%0A%20%20%20%20C%20--%3E%7CNo%7C%20E%5BResult%5D%3B%0A%20%20%20%20D%20--%3E%7CYes%7C%20E%3B%0A%20%20%20%20D%20--%3E%7CNo%7C%20F%5BResult%5D%3B%0A%20%20%20%20E%20--%3E%7CSuccess%7C%20G%3B%0A%20%20%20%20F%20--%3E%7CFailure%7C%20H%3B%0A%20%20%20%20G%20--%3E%20Stop%3B%0A%20%20%20%20H%20--%3E%20Stop%3B
@@ -157,7 +115,7 @@ async function initializePuppeteer() {
 }
 
 
-export async function renderCode(page: Page) {
+async function renderCode(page: Page) {
   try {
     await page.waitForSelector('#container', {
 
